bf2a8e1967fb95561e795ec31a9190875fa143b3
Aakibgithub <101191261+Aakibgithuber@users.noreply.github.com>
2023-01-22 18:47:28 +0530

    Update jenkins.groovy

--- todo/django-todo-cicd-develop/jenkins.groovy
+++ todo/django-todo-cicd-develop/jenkins.groovy
@@ -1,24 +1,39 @@
 pipeline {
     agent any
-    stages {
-        stage('Build and Tag Image') {
+ stages {
+  stage('Docker Build and Tag') {
+           steps {
+              
+                sh 'docker build -t todoapp:latest .' 
+                  sh 'docker tag todoapp newtodoapp/todoapp:latest'
+                sh 'docker tag todoapp newtodoapp/todoapp:$BUILD_NUMBER'
+               
+          }
+        }
+     
+  stage('Publish image to Docker Hub') {
+          
             steps {
-                sh 'docker build -t myimage:latest .'
-                sh 'docker tag myimage:latest myrepo/myimage:latest'
+        withDockerRegistry([ credentialsId: "dockerHub", url: "" ]) {
+          sh  'docker push newtodoapp/todoapp:latest'
+          sh  'docker push newtodoapp/todoapp:$BUILD_NUMBER' 
+        }
+                  
+          }
+        }
+     
+      stage('Run Docker container on Jenkins Agent') {
+             
+            steps {
+                sh "docker run -d -p 4030:80 newtodoapp/todoapp"
+                
             }
         }
-        stage('Push to Dockerhub') {
+ stage('Run Docker container on remote hosts') {
+             
             steps {
-                withCredentials([usernamePassword(credentialsId: 'dockerhub', usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD')]) {
-                    sh "docker login -u ${USERNAME} -p ${PASSWORD}"
-                    sh 'docker push myrepo/myimage:latest'
-                }
-            }
-        }
-        stage('Pull and Run Image') {
-            steps {
-                sh 'docker pull myrepo/myimage:latest'
-                sh 'docker run -d --name mycontainer myrepo/myimage:latest'
+                sh "docker -H ssh://jenkins@192.168.49.1 run -d -p 4001:80 newtodoapp/todoapp"
+ 
             }
         }
     }
